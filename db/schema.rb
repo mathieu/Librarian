b222726e4b9efda14b876c2daf6489f32d620e3c
Mathieu MARACHE <mathieu.marache@cstb.fr>
2013-11-13 16:50:08 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113135716) do
+ActiveRecord::Schema.define(version: 20131113154615) do
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.date     "registered"
+    t.boolean  "available"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
