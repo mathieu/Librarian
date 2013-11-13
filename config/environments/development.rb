5e9a454edda2be458e632e939c996ae83eaf1fa6
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2013-11-13 14:54:34 +0100

    Add default devise beahaviour

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Librarian::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Define Host for mailer
+  config.action_mailer.default_url_options = { :host => 'librarian.cstb.fr' }
 end
