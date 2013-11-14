55dcb781329aeaff2ab93ea09e4eb96eccd0d9e3
Mathieu Marache <mathieu@marache.com>
2013-11-14 14:34:35 +0100

    playing with cover location naming

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -12,7 +12,11 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.isbn}"
+  end
+
+  def filename
+    "cover#{File.extname(original_filename)}" if original_filename
   end
 
   # Process files as they are uploaded:
