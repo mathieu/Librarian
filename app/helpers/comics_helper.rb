3d4f40e16139e82f08460cd22734e184f9d0686a
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2013-11-15 00:01:59 +0100

    Add helper to display book's authors, use it in grid and list

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,2 +1,10 @@
 module ComicsHelper
+  def display_authors(authors)
+    name =''
+    authors.each do |author|
+      name += author.name + ', '
+    end
+
+    name.strip.end_with?(',') ? name.strip[0..-2] : name.strip
+  end
 end
