8f330b4c2f80601c6c7749c432afbcd4e1a95d45
Mathieu Marache <mathieu@marache.com>
2013-11-15 08:37:44 +0100

    annotate and created fixtures for tests

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -4,7 +4,6 @@
 #
 #  id            :integer          not null, primary key
 #  title         :string(255)
-#  author        :string(255)
 #  registered    :date
 #  available     :boolean
 #  created_at    :datetime
@@ -12,6 +11,7 @@
 #  isbn          :string(13)
 #  collection_id :integer
 #  cover         :string(255)
+#  author_names  :string(255)
 #
 
 class Comic < Item
