b62934570853ba772bfbf1327e9ece11ca5465de
Mathieu Marache <mathieu@marache.com>
2013-11-15 08:37:44 +0100

    annotate and created fixtures for tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Author < ActiveRecord::Base
 
   has_and_belongs_to_many :items
